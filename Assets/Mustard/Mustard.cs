1100c43b875c6365f20b09b002a17e817a8a3fce
kulacs <gyurcsanyfecoka@gmail.com>
2025-10-26 11:30:45 +0100

    microwave

---
+++ Assets/Mustard/Mustard.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class Mustard : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    
+    void Explode()
+    {
+        Debug.Log("Mustard explosion effect triggered!");
+        // Switch sprites to exlpoded version
+    }
+}
--- Assets/Prefabs/InteractableObjects/Stationery/MicrowaveHandle.cs
+++ Assets/Prefabs/InteractableObjects/Stationery/MicrowaveHandle.cs
@@ -2,17 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MicrowaveHandle : MonoBehaviour
+public class MicrowaveHandle : InteractableBase
 {
-    // Start is called before the first frame update
-    void Start()
+    public bool hasFork = false;
+    
+    public override void Interact()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Debug.Log("Microwave Handle Turned!!!");
     }
 }
